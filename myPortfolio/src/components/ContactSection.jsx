7b360c61a12efac1d89b2d531538524c83d75227
utshobbose <utshob.bose@g.bracu.ac.bd>
2025-07-07 13:22:57 +0600

    Update contact

--- myPortfolio/src/components/ContactSection.jsx
+++ myPortfolio/src/components/ContactSection.jsx
@@ -92,7 +92,7 @@ export const ContactSection = () => {
             <div className="pt-8">
               <h4 className="font-medium mb-4"> Connect With Me</h4>
               <div className="flex space-x-4 justify-center">
-                <a href="#" target="_blank">
+                <a href="https://www.linkedin.com/in/utshob-bose-135812373/" target="_blank">
                   <Linkedin />
                 </a>
                 {/* <a href="#" target="_blank">
