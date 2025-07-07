ad9c7b1fbe250f3fb73cb6b751c5187be451508c
utshobbose <utshob.bose@g.bracu.ac.bd>
2025-07-08 02:09:39 +0600

    CV Updated

--- myPortfolio/src/components/AboutSection.jsx
+++ myPortfolio/src/components/AboutSection.jsx
@@ -33,7 +33,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="/Bose-Resume.pdf"
+                href="/Utshob Bose_CV.pdf"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
