cf6f8a9252b9ab55842caf2af4fc17be146fdf27
utshobbose <boseadidev1@gmail.com>
2026-01-27 14:18:05 +0600

    CV updated

--- myPortfolio/src/components/AboutSection.jsx
+++ myPortfolio/src/components/AboutSection.jsx
@@ -33,7 +33,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="/UTSHOB_BOSE_CV_ATS.pdf"
+                href="/UTSHOB BOSE_Resume(ATS).pdf"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
                 target="_blanks"
               >
